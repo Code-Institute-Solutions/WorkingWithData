9941e0884935caaa693b3afe257d1d2d30a366a4
aaron <aaron@codeinstitute.net>
2018-11-22 12:00:02 +0000

    Fix typo in solution

--- 06-SQLInAction/12-the_insert_statement/video_solution/queries.sql
+++ 06-SQLInAction/12-the_insert_statement/video_solution/queries.sql
@@ -1,2 +1,28 @@
+/*
+Create a dummy `MediaType`
+*/
 INSERT INTO MediaType (Name)
 VALUES ("Test Media 1");
+
+/*
+Create a new `Album` record with a name of "Boy" and an
+ArtistId of 150. This ArtistId is for U2
+*/
+INSERT INTO Album (Title, ArtistId)
+VALUES ("Boy", 150);
+
+/*
+Get all of the relevant information that we need to enter all of
+tracks for U2's "Boy" album
+*/
+SELECT AlbumId FROM Album WHERE Title = "Boy";
+
+SELECT MediaTypeId FROM MediaType WHERE Name = "Protected AAC audio file";
+
+SELECT GenreId FROM Genre WHERE Name = "Rock";
+
+/*
+Use the above information to create the new tracks
+*/
+INSERT INTO Track (Name, AlbumId, MediaTypeId, GenreId, Composer, Milliseconds, Bytes, UnitPrice)
+VALUES ("I Will Follow", 348, 2, 1, "U2", 220000, 1234, 0.99);
